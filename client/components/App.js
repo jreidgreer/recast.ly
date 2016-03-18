76719b71caec1ca82c32b1a1b3e2550700fc6f29
jreidgreer <jreid.greer@gmail.com>
2016-03-18 16:55:04 -0700

    Refactor VideoList & VideoListEntry to be ES6 React Component classes

--- client/components/App.js
+++ client/components/App.js
@@ -1,16 +1,20 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      videos: props.videoData,
+      currentVideo: props.videoData[0]
+    };
   }
 
   render() {
     return (<div>
               <Nav />
               <div className="col-md-7">
-                <VideoPlayer video={this.props.videoData[0]} />
+                <VideoPlayer video={this.state.currentVideo} />
               </div>
               <div className="col-md-5">
-                <VideoList videoList={this.props.videoData} />
+                <VideoList videoList={this.state.videos} />
               </div>
             </div>);
   }
--- client/components/VideoList.js
+++ client/components/VideoList.js
@@ -1,11 +1,18 @@
-var VideoList = (props) => (
-  <div className="video-list media">
-    <VideoListEntry video={props.videoList[0]} />
-    <VideoListEntry video={props.videoList[1]} />
-    <VideoListEntry video={props.videoList[2]} />
-    <VideoListEntry video={props.videoList[3]} />
-    <VideoListEntry video={props.videoList[4]} />
-  </div>
-);
+class VideoList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="video-list media">
+        <VideoListEntry video={props.videoList[0]} />
+        <VideoListEntry video={props.videoList[1]} />
+        <VideoListEntry video={props.videoList[2]} />
+        <VideoListEntry video={props.videoList[3]} />
+        <VideoListEntry video={props.videoList[4]} />
+      </div>);
+  }
+}
 
 window.VideoList = VideoList;
