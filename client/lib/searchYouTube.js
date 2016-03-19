dfc936a92c3adb1114a84fc30cbb354ed86cac98
jreidgreer <jreid.greer@gmail.com>
2016-03-18 19:30:30 -0700

    Complete YouTube AJAX search script

--- client/lib/searchYouTube.js
+++ client/lib/searchYouTube.js
@@ -1,5 +1,14 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  var maxResults = options.max || 5;
+  $.ajax('https://www.googleapis.com/youtube/v3/search&key=' + options.key + '&q=' + options.query + '&maxResults=' + maxResults + '&videoEmbeddable=true', {
+    success: function(data) {
+      callback(data);
+    },
+    error: function(data) {
+      console.error('AJAX Fail. No bueno.', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
+
